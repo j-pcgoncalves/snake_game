00839c8815d79784e98adfe6eacc29418726eaba
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2024-05-13 18:56:40 +0100

    generate randomly the food piece

--- script.js
+++ script.js
@@ -6,19 +6,24 @@ const score = document.getElementById("score");
 const highScoreText = document.getElementById("highScore");
 
 // Define game variables
+const gridSize = 20;
 let snake = [{ x: 10, y: 10 }];
+let food = generateFood();
+let gameInterval;
 let gameSpeedDelay = 200;
 let gameStarted = false;
 
 // Draw Game Map, snake and food
 function draw() {
-    board.innerHTML = " ";
+    board.innerHTML = "";
     drawSnake();
+    drawFood();
 }
 
 function drawSnake() {
     snake.forEach((segment) => {
         const snakeElement = createGameElement("div", "snake");
+        setPosition(snakeElement, segment);
 
         board.appendChild(snakeElement);
     })
@@ -29,6 +34,28 @@ function createGameElement(tag, className) {
     const element = document.createElement(tag);
     element.className = className;
     return element;
+}
+
+// Set the position of snake or food
+function setPosition(element, position) {
+    element.style.gridColumn = position.x;
+    element.style.gridRow = position.y;
+}
+
+function drawFood() {
+    if (gameStarted) {
+        const foodElement = createGameElement("div", "food");
+        setPosition(foodElement, food);
+
+        board.appendChild(foodElement);
+    }
+}
+
+function generateFood() {
+    const x = Math.floor(Math.random() * gridSize) + 1;
+    const y = Math.floor(Math.random() * gridSize) + 1;
+    
+    return { x, y };
 }
 
 function startGame() {
